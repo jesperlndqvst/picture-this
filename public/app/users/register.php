eaa0c86975ff1abd4416e973caec9af42b566aaa
Jesper <jesper.lundqvist@gmail.com>
2019-12-10 18:02:27 +0100

    created error message logic which displays if username or email is already taken

--- public/app/users/register.php
+++ public/app/users/register.php
@@ -9,13 +9,30 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
+    // Checks if email or username is taken
+    $statement = $pdo->prepare('SELECT username, email FROM users');
+    $statement->execute();
+    $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($users as $user) {
+        if ($user['username'] === $username || $user['email'] === $email) {
+            if ($user['username'] === $username) {
+                $_SESSION['username_error'] = 'Username is already taken';
+            }
+            if ($user['email'] === $email) {
+                $_SESSION['email_error'] = 'Email is already taken';
+            }
+            redirect('/register.php');
+        }
+    }
+
+    // Inserts user data to database
     $query = 'INSERT INTO users (username, email, password) VALUES (:username, :email, :password)';
     $statement = $pdo->prepare($query);
     $statement->bindParam(':username', $username, PDO::PARAM_STR);
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->bindParam(':password', $password, PDO::PARAM_STR);
     $statement->execute();
-    
 }
 
-redirect('/');
+redirect('/login.php');
