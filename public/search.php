4a50e3a59c6c3ff3d507a8fb240294abb4857dda
Jesper <jesper.lundqvist@gmail.com>
2019-12-18 18:36:30 +0100

    changed file structure for searching

---
+++ public/search.php
@@ -0,0 +1,27 @@
+<?php require __DIR__ . '/views/header.php'; ?>
+
+
+        <form action="/app/users/search.php" method="get">
+            <label for="search">Search</label>
+            <input type="text" name="search" required>
+            <button type="submit">Search</button>
+        </form>
+
+
+    <?php if (isset($_SESSION['searchResults'])) : ?>
+        <?php $searchResults = $_SESSION['searchResults'] ?>
+        <?php foreach ($searchResults as $searchResult) : ?>
+            <a href="/profile.php?id=<?= $searchResult['id'] ?>&username=<?= $searchResult['username'] ?>"><?= $searchResult['username'] ?></a>
+        <?php endforeach; ?>
+        <?php unset($_SESSION['searchResults']); ?>
+<?php endif; ?>
+
+
+<?php if (isset($_SESSION['errors'])) : ?>
+    <?php foreach ($_SESSION['errors'] as $error) : ?>
+        <p><?= $error ?></p>
+    <?php endforeach; ?>
+    <?php unset($_SESSION['errors']); ?>
+<?php endif; ?>
+
+<?php require __DIR__ . '/views/footer.php'; ?> 
--- public/views/navigation.php
+++ public/views/navigation.php
@@ -27,31 +27,7 @@
         </li>
 
 
-        <?php if (isset($_SESSION['user'])) : ?>
-            <li>
-                <form action="/app/users/search.php" method="get">
-                    <label for="search">Search</label>
-                    <input type="text" name="search" required>
-                    <button type="submit">Search</button>
-                </form>
-            <?php endif; ?>
 
-            <?php if (isset($_SESSION['searchResults'])) : ?>
-                <?php $searchResults = $_SESSION['searchResults'] ?>
-                <?php foreach ($searchResults as $searchResult) : ?>
-                    <a href="/../profile.php?id=<?= $searchResult['id'] ?>&username=<?= $searchResult['username'] ?>"><?= $searchResult['username'] ?></a>
-                <?php endforeach; ?>
-                <?php unset($_SESSION['searchResults']); ?>
-            </li>
-        <?php endif; ?>
-
-
-        <?php if (isset($_SESSION['errors'])) : ?>
-            <?php foreach ($_SESSION['errors'] as $error) : ?>
-                <p><?= $error ?></p>
-            <?php endforeach; ?>
-            <?php unset($_SESSION['errors']); ?>
-        <?php endif; ?>
 
     </ul>
 </nav>
