cee041a9378b0b13126527deaa9a1cd926edcb00
Jesper <jesper.lundqvist@gmail.com>
2019-12-19 14:44:55 +0100

    fixed small bug where user were able to unfollow him/herself

--- public/app/users/follow.php
+++ public/app/users/follow.php
@@ -9,16 +9,19 @@ if (isset($_GET['id'])) {
     $followId = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
 
     // Checks if user is in the followers table
-    $query = 'SELECT user_id FROM followers WHERE user_id = :userId AND follow_id = :followId';
+    $query = 'SELECT user_id FROM followers
+    WHERE user_id = :userId AND follow_id = :followId AND user_id != follow_id';
     $statement = $pdo->prepare($query);
     $statement->bindParam(':userId', $userId, PDO::PARAM_INT);
     $statement->bindParam(':followId', $followId, PDO::PARAM_INT);
     $statement->execute();
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
+    // Unfollow user
     if ($user) {
         displayMessage('User Unfollowed');
-        $query = 'DELETE FROM followers WHERE user_id = :userId AND follow_id = :followId';
+        $query = 'DELETE FROM followers
+        WHERE user_id = :userId AND follow_id = :followId';
         $statement = $pdo->prepare($query);
         $statement->bindParam(':userId', $userId, PDO::PARAM_INT);
         $statement->bindParam(':followId', $followId, PDO::PARAM_INT);
@@ -26,6 +29,7 @@ if (isset($_GET['id'])) {
         redirect('/../../profile.php?id=' . $_GET['id'] . '&username=' . $_GET['username']);
     }
 
+    // Follow user
     $query = 'INSERT INTO followers (user_id, follow_id)
     VALUES (:userId, :followId)';
     $statement = $pdo->prepare($query);
