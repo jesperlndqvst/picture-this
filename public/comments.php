78753dbd78e1d097d4298da0329f635d062494e1
Jesper <jesper.lundqvist@gmail.com>
2020-01-04 13:43:57 +0100

    added css for comments

--- public/comments.php
+++ public/comments.php
@@ -10,31 +10,23 @@
 
 <article class="comments">
     <form class="form form--comments" action="/app/posts/comments.php?id=<?= $postId ?>" method="post">
-        <a href="/#<?= $postId ?>">Return</a>
         <input class="form__input" type="text" name="comment" placeholder="Comment" autocomplete="off" required>
         <button class="btn btn--lg" type="submit" name="submit">Comment</button>
     </form>
-
+    <a href="/#<?= $postId ?>"><button class="btn btn--lg" type="submit" name="submit">Return</button></a>
 
     <?php $comments = getComments($postId, $pdo) ?>
     <?php foreach ($comments as $comment) : ?>
         <div class="comments-content">
             <img src="app/uploads/avatars/<?= $comment['avatar'] ?>" alt="User avatar">
-            <p>
-                <span class="text-bold"><?= $comment['username'] ?></span>
-                <?= $comment['comment'] ?>
-            </p>
-            <p><?= $comment['date'] ?></p>
+            <div class="comments-content__text">
+                <p class="text-bold"><?= $comment['username'] ?></p>
+                <p><?= $comment['comment'] ?></p>
+                <p><?= $comment['date'] ?></p>
+            </div>
         </div>
     <?php endforeach; ?>
 
-
-
-
 </article>
-
-
-
-
 
 <?php require __DIR__ . '/views/footer.php'; ?>
